6c7787307c6177289a5ea12c6d297cc559f9f1d8
gjbundy <gbundy@microsoft.com>
2022-12-19 11:40:23 -0800

    Update New_StudioCrew.js

--- New_StudioCrew.js
+++ New_StudioCrew.js
@@ -6,8 +6,8 @@ Parameter 2 - PrimaryEntityTypeName
 Parameter 3 - FirstPrimaryItemId
 Parameter 4 - PrimaryControl (this is where we get the context of the form)
 Parameter 5 - SelectedControl (this is the subgrid we are starting from)
-Parameter 6 - A number value corresponding to the mtt_entrytype column in the Crew table
-Parameter 7 - A number value corresponding to the mtt_whichedit column in the Crew table to determine review or final edit. 0=Review, 1=Final
+Parameter 6 - A number value corresponding to the test_entrytype column in the Crew table
+Parameter 7 - A number value corresponding to the test_whichedit column in the Crew table to determine review or final edit. 0=Review, 1=Final
 */
 
 function addNewFromSubgrid(gridEntityName, parentEntityName, parentEntityId, primaryControl, gridControl, entryType, editType) {
